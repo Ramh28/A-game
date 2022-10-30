3dcc52f04b65acf2b925401699a9e1dff08ad5d7
Wladimir <wladimirss14@gmail.com>
2022-10-30 00:39:30 -0400

    funcionalidad basica

---
+++ src/vuejs/main.js
@@ -0,0 +1,45 @@
+const { createApp } = Vue;
+
+const app = Vue.createApp({
+  data() {
+    return {
+      money: 0,
+      profitMultiplier: 1,
+
+      income: 1,
+      incomeCost: 1,
+      incomeLevel: 1,
+      passiveIncome: 0,
+      passiveIncomeCost: 1,
+      passiveIncomeLevel: 0,
+
+      multiplier: 1,
+
+      expenses: 0,
+      citycetns: 0
+    }
+  },
+  methods: {
+    makeMoney(){
+      this.money += this.income;
+    },
+    incomeIncrement(){
+      this.money -= this.incomeCost;
+      this.incomeLevel ++;
+      this.incomeCost += (this.incomeCost*0.07);
+      this.income = this.incomeLevel;
+    },
+    passiveIncomeIncrement(){
+      this.money -= this.passiveIncomeCost;
+      this.passiveIncomeLevel ++;
+      this.passiveIncomeCost += (this.passiveIncomeCost*0.07);
+      this.passiveIncome = this.passiveIncomeLevel;
+    }
+  },
+  mounted() {
+    this.interval = setInterval(() => {
+      this.money += this.profitMultiplier * this.passiveIncome; 
+    }, 1000);
+  }
+})
+app.mount("#app")
